55294e3d31cb3d5954c87e5a92d7c57b0b341a7c
Abby Benjamin <ablizben@gmail.com>
2021-02-10 22:46:34 -0500

    initial html, css, and js files

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Add squares
+
+const squares = document.querySelector('.squares');
+for (var i = 1; i < 365; i++) {
+    const level = Math.floor(Math.random() * 3);
+    squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+}
