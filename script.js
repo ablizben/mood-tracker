00f053d977244672988667db399e71c6c25a88ea
Abby Benjamin <ablizben@gmail.com>
2021-02-18 10:28:25 -0500

    removing initial grid colors and adding modal window

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ for (var i = 1; i < 365; i++) {
     const level = Math.floor(Math.random() * 3);
     squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
 }
+
+//window to input mood data
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+// const btnOpenSquare = document.querySelector('.li');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+//open data input window
+squares.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+})
+
+//close data input window
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+
+})
