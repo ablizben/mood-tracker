2208522dd026addd7741afe35f4c3d5b121e8d7b
Abby Benjamin <ablizben@gmail.com>
2021-02-24 20:34:41 -0500

    adding mDmanipulating DOM elements

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,17 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        db.collection('guides').get().then(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        });
+    } else {
+        setupUI();
+        setupGuides([]);
+    }
+})
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 
@@ -25,4 +39,24 @@ logout.addEventListener('click', (e) => {
     auth.signOut().then(() => {
         console.log('user signed out');
     })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+
 });
